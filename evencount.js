bb124199735bebc181e14de77ab3461183b8534c
SUBASRI-D <subasridurai07@gmail.com>
2024-08-13 23:19:17 +0530

    added evencount program

---
+++ evencount.js
@@ -0,0 +1,14 @@
+function evencount(){
+    let arr=[10,2,3,5,60]
+    count=0
+    for(i=0;i<arr.length;i++){
+        if(arr[i]%2==0){
+        count+=1
+
+    }
+    }
+    console.log(count)
+}
+evencount()
+
+
