9cd3fc0294a49c86035a6d653030f2dc30bb6975
SUBASRI-D <subasridurai07@gmail.com>
2024-08-15 00:57:14 +0530

    added checknum program

---
+++ checknum.js
@@ -0,0 +1,13 @@
+let arr=[1,4,5,7,6]
+let value=3
+for(let i=0;i<=arr.length;i++){
+    if(arr[i]==value){
+        console.log("True")
+        break;
+    }
+    else{
+        console.log("False")
+        break;
+        
+    }
+}
