86f727d56bef613ffb61c843568bf2b07d71ac41
SUBASRI-D <subasridurai07@gmail.com>
2024-08-16 23:15:22 +0530

    changed the logic in the program

--- greaternum.js
+++ greaternum.js
@@ -1,21 +1,10 @@
-let arr=[22,11,20,2,4];
+let arr=[2,11,20,24,4];
 let target_val=20;
 
-for(let i=0;i<arr.length;i++){
-    if(arr[i]>target_val){
-        console.log("First num "+arr[i]+" grater than"+target_val)
-        break;
-    }
-    if(arr[i]==target_val){
-         console.log("First num "+arr[i]+"equal to "+target_val)
-         break
-        
-    }
-    else{
-        
-        console.log("First num "+arr[i]+"less than"+target_val)
-    
-        break;
+for(let i=0;i<arr.length;i++){  
+      if(arr[i]>target_val){
+        console.log(arr[i]);
+        break
 
     }
-}
+}   
