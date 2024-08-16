96af04abad0723d2c7c00e6607ac0c12a6a4cdac
SUBASRI-D <subasridurai07@gmail.com>
2024-08-16 21:29:50 +0530

    Added wordcount program

---
+++ wordscount.js
@@ -0,0 +1,10 @@
+let String = prompt("Enter the word");
+let string = String.trim();
+let len = string.length;
+let count=0;
+for(let i=0;i<len;i++)
+{
+   count+=1 
+}
+console.log("Word count in string:",count)
+
