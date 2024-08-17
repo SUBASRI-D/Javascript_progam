9a0b583bf062ae81ee4d2a411684d88c0f15ac1f
SUBASRI-D <subasridurai07@gmail.com>
2024-08-17 21:57:44 +0530

    added palindrome program

---
+++ palindrome.js
@@ -0,0 +1,16 @@
+let string1="AmmA";
+let string=string1.trim();
+let len=string.length;
+let ispalindrome=true;
+for(i=0;i<len/2;i++){
+    if(string[i]!==string[len-i-1]){
+        ispalindrome=false
+    }
+}
+if(ispalindrome){
+    console.log("palindrome")
+}
+else{
+    console.log("Not palindrome")
+
+}
