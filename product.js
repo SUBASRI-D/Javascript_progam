4903c5b89d7531c5048675a69b93d2d98cd5e397
SUBASRI-D <subasridurai07@gmail.com>
2024-08-21 23:02:44 +0530

    added the ifcondition

--- product.js
+++ product.js
@@ -1,6 +1,13 @@
-let arr=[1,2,3,4,5]
+let arr=[1,0,2,3,4,5]
 let product=1
 for(let i=0;i<arr.length;i++){
-    product*=arr[i]
+    if(arr[i]==0){
+        continue
+    }
+    else{
+        product*=arr[i]
+
+    }
+   
 }
 console.log(product)
